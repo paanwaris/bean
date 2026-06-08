8647047c03449daba899af3ce54358ab6758a8b9
Paanwaris Paansri <paanwaris_04@hotmail.com>
2026-06-08 12:18:31 -0400

    Delete methods.R

--- R/methods.R
+++
@@ -1,6 +0,0 @@
-# The predict() method for bean_ellipsoid objects was removed in bean 0.2.1.
-# It will return in a future release once the nicheR package (which provides
-# the canonical implementation) is available on CRAN.
-#
-# This file is intentionally empty; delete it before running
-# devtools::document().
